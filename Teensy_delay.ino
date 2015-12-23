b094732bb3601fda0361f622a7aea31b870ff647
Dries <driesgoddefroy@MacBook-Pro-van-Dries.local>
2015-12-23 22:59:32 +0100

    First

---
+++ Teensy_delay.ino
@@ -0,0 +1,24 @@
+// Teensy 2.0 has the LED on pin 11
+// Teensy++ 2.0 has the LED on pin 6
+// Teensy 3.0 has the LED on pin 13
+const int ledPin = 11;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  void startupdelay();
+}
+void loop() { 
+}
+
+void startupdelay() {
+  digitalWrite(ledPin, HIGH);
+  delay(6000);
+  digitalWrite(ledPin, LOW);
+  for (int i=10; i == 0; i--){ 
+    digitalWrite(ledPin, HIGH);
+    delay(i*10); 
+    digitalWrite(ledPin, LOW);
+    delay(i*10);
+  }
+}
+
